4ff61aeda1caac5d01acae803239c64228a2bccc
nitay737 <nitay737@gmail.com>
2026-01-14 18:36:39 +0200

    added StompServer

--- server/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
+++ server/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
@@ -2,12 +2,30 @@ package bgu.spl.net.impl.stomp;
 
 import com.google.common.base.Supplier;
 
+import bgu.spl.net.impl.echo.EchoProtocol;
+import bgu.spl.net.impl.echo.LineMessageEncoderDecoder;
+import bgu.spl.net.srv.BaseServer;
+import bgu.spl.net.srv.BlockingConnectionHandler;
 import bgu.spl.net.srv.Reactor;
 import bgu.spl.net.srv.Server;
 
 public class StompServer {
 
     public static void main(String[] args) {
-        
+        Server<StompMessage> server = null;
+        if(args[1].equals("reactor")){
+            server = new Reactor<StompMessage>(5, Integer.parseInt(args[0]), () -> new StompMessagingProtocolImp(), () -> new MessageEncoderDecoderImpl());
+
+        }else if(args[1].equals("tpc"))
+        {
+            server = new BaseServer<StompMessage>(Integer.parseInt(args[0]),() -> new StompMessagingProtocolImp(), () -> new MessageEncoderDecoderImpl()) {
+            @Override
+            protected void execute(BlockingConnectionHandler<StompMessage>  handler) {
+                new Thread(handler).start();
+            }
+        };
+        }
+
+        server.serve();
     }
 }
